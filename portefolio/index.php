981f6c87031dc01a70c4995ff446617c50df50b4
Léo Trentesaux <leotr@DESKTOP-O3364KP.BTSINFO.LOCAL>
2022-12-17 10:32:24 +0100

    test

---
+++ portefolio/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Maquette de site web</title>
+    <meta name="description" content="Maquette de site web avec PHP et JavaScript">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Maquette de site web</h1>
+    <p>Bienvenue sur ma maquette de site web.</p>
+    
+    <?php
+        // Code PHP pour afficher la date et l'heure actuelles
+        echo "Date et heure actuelles : " . date("Y-m-d H:i:s");
+    ?>
+    
+    <script>
+        // Code JavaScript pour afficher un message d'alerte
+        alert("Bienvenue sur ma maquette de site web !");
+    </script>
+</body>
+</html>
