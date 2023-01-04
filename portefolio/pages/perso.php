64a97fcbe559a592aee3574d41002dc715e4b746
Léo Trentesaux <leotr@DESKTOP-O3364KP.BTSINFO.LOCAL>
2023-01-04 13:17:11 +0100

    Maquette V7

--- portefolio/pages/perso.php
+++ portefolio/pages/perso.php
@@ -1,2 +1,19 @@
 <?php
     include "header.php";?>
+
+<table>
+    <td>
+    <?php
+            //Liste des contenus avec les redirections, avec une id j
+
+
+            //Crée une nouvelle ligne avec pour id i, et dont les cases prennent pour id k 1, 2 ou 3
+
+
+            //Scan la ligne sur la colonne au dessus. Si la case 1 de la ligne i-1 est vide, alors sur la case
+// 1 de la ligne i on ajoute le contenu. Sinon, on affiche le contenu dans la case 3
+   
+
+        ?>
+    </td>
+</table>
--- portefolio/pages/professionel.php
+++ portefolio/pages/professionel.php
@@ -1,2 +1,31 @@
 <?php
     include "header.php";?>
+
+<table>
+    <?php
+        
+
+$array= array( "<div id='case'>
+                        <div id='caseh'> <a href='projets/luxuton.php'> Luxuton </a> </div>
+                        <div id='caseb'> Luxuton est une entreprise fictive qui revisite des objets du quotidien en objet de luxes. </div>
+                        </div>", "<div id='case'>
+                        <div id='caseh'> N/A </div>
+                        <div id='caseb'> N/A </div>
+                        </div>", "<div id='case'>
+                        <div id='caseh'> N/A </div>
+                        <div id='caseb'> N/A </div>
+                        </div>");
+            $j=count($array);
+            for($i=0;$i<$j; $i++){
+                if($i%2==0){
+                    echo("<tr> <td id='cote1'>".$array[$i]."</td><td id='milieu'></td><td id='cote2'></td></tr>");
+                }
+                else{
+                    echo("<tr> <td id='cote1'></td><td id='milieu'></td><td id='cote2'>".$array[$i]."</td></tr>");
+                
+                }
+            }
+
+        ?>
+    </td>
+</table>
